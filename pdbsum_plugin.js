fc7a1723d92014c4e647598bb9c11ae90ab941ab
Jagoda Jabłońska <Jagoda Jaboska>
2016-08-25 09:14:00 +0100

    Fixed bug and added example input files

--- pdbsum_plugin.js
+++ pdbsum_plugin.js
@@ -1,25 +1,23 @@
-var color_dict = {"A":'#cc66ff', "B": '#ff0000', "C": '#cc9900', "D":'#cc9900',
-				 "E":'#ff69b4', "F":'#0000ff', "G":'#00ff00', "H":'#33ccff', 
-				 "I":'#ffff00', "J":'#cc66ff', "K":'#ff0000', "L":'#cc9900', 
-				 "M":'#ff69b4', "N":'#0000ff', "O":'#00ff00', "P":'#33ccff', 
-				 "Q":'#ffff00', "R":'#cc66ff', "S":'#ff0000', "T": '#cc9900', 
-				 "U":'#ff69b4', "V":'#0000ff', "W": '#00ff00',"X":'#33ccff', 
-				 "Y":'#ffff00', "Z":'#cc66ff', "1":'#ff0000', "2":'#cc9900', 
-				 "3":'#ff69b4', "4":'#0000ff', "5":'#00ff00', "6":'#33ccff', 
-				 " ":'#ffff00', "7":'#cc66ff', "8":'#ff0000', "9":'#cc9900',
-				 "0":'#ff69b4', "!":'#0000ff', "#":'#00ff00', "_":'#33ccff', 
-				 "-":'#ffff00', "=":'#cc66ff', ":":'#ff0000', "<":'#cc9900', 
-				 ">":'#ff69b4', "|":'#0000ff'}
+var color_dict = {'A':'#cc66ff', 'B':'#ff0000', 'C':'#cc9900', 'D':'#ff69b4', 'E':'#0000ff', 
+				'F':'#00ff00', 'G':'#33ccff', 'H':'#ffff00', 'I':'#cc66ff', 'J':'#ff0000', 
+				'K':'#cc9900', 'L':'#ff69b4', 'M':'#0000ff', 'N':'#00ff00', 'O':'#33ccff', 
+				'P':'#ffff00', 'Q':'#cc66ff', 'R':'#ff0000', 'S':'#cc9900', 'T':'#ff69b4', 
+				'U':'#0000ff', 'V':'#00ff00', 'W':'#33ccff', 'X':'#ffff00', 'Y':'#cc66ff', 
+				'Z':'#ff0000', '1':'#cc9900', '2':'#ff69b4', '3':'#0000ff', '4':'#00ff00', 
+				'5':'#33ccff', '6':'#ffff00', ' ':'#cc66ff', '7':'#ff0000', '8':'#cc9900', 
+				'9':'#ff69b4', '0':'#0000ff', '!':'#00ff00', '#':'#33ccff', '_':'#ffff00', 
+				'-':'#cc66ff', '=':'#ff0000', ':':'#cc9900', '<':'#ff69b4', '>':'#0000ff', 
+				'|':'#00ff00'};
 
 var glviewer = null;
 var labels = [];
-var pdb_file = 'file:///C:/Users/jagoda/Desktop/disastr_repo/3fvq.pdb';
-var dfn_file = 'file:///C:/Users/jagoda/Desktop/disastr_repo/rasmol.dfn';
+var pdb_file = 'file:///C:/Users/jagoda/Desktop/PDBsum_repo/3aiy.pdb';
+var dfn_file = 'file:///C:/Users/jagoda/Desktop/PDBsum_repo/rasmol.dfn';
 
 var apply_styles = function(viewer){
 	var chains_prot = [];
 	var chains_nuc = [];
-	var ligands_res = {};
+	var ligands_res = [];
 	var metals_res = [];
 
 	var dfn_f = readFile(dfn_file);
@@ -31,9 +29,10 @@ var apply_styles = function(viewer){
 		} else if (splitted[i][0] === "N"){
 			chains_nuc.push(splitted[i][1]);
 		} else if (splitted[i][0] === "l"){
-			resid = splitted[i].split(" ")[2].split("(")[0] // need for regexp :-(
-			chain = splitted[i].split(" ")[2].split("(")[1][0]
-			ligands_res[resid] = chain;
+			var regExp = /\(([^)]+)\)/;
+			resid = splitted[i].split(" ")[2].split("(")[0]
+			chain = regExp.exec(splitted[i])[1];
+			ligands_res.push([resid,chain]);
 		} else if (splitted[i][0] === "M"){
 			metals_res.push(splitted[i].slice(1, splitted.length).split(" ")[0]);
 		}
@@ -42,10 +41,13 @@ var apply_styles = function(viewer){
 	for (var i = 0; i < chains_prot.length; i++) {
 		glviewer.setStyle({chain:chains_prot[i]}, {cartoon:{color:color_dict[chains_prot[i]]}}); // sel chain
 	}
-	glviewer.setStyle({chain:chains_nuc}, {stick:{}}); // nucleic acid
 
-	for (key in ligands_res) {
-		glviewer.setStyle({chain:ligands_res[key], resi:key}, {sphere:{}}); // ligands
+	for (var i = 0; i < chains_nuc.length; i++) {
+		glviewer.setStyle({chain:chains_nuc[i]}, {stick:{color:color_dict[chains_nuc[i]]}}); // nucleic acid
+	}
+
+	for (var i = 0; i < ligands_res.length; i++) {
+		glviewer.setStyle({chain:ligands_res[i][1], resi:ligands_res[i][0]}, {sphere:{}}); // ligands
 	}
 
 	for (var i = 0; i < metals_res.length; i++) {
